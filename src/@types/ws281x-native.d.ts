63d3cc6d28a63909e7ecbd2ee920d0d16f7ef087
三浦勇 <c01150788f@edu.teu.ac.jp>
2020-04-11 18:44:16 +0900

    add d.ts

---
+++ src/@types/ws281x-native.d.ts
@@ -0,0 +1,8 @@
+// https://github.com/beyondscreen/node-rpi-ws281x-native
+
+export function init(numLeds: number, options?: Object): void;
+export function setIndexMapping(map: Array<number>): void;
+export function render(data: Uint32Array): Uint32Array;
+export function setBrightness(brightness: number): void;
+export function reset(): void;
+export function isStub(): boolean;
