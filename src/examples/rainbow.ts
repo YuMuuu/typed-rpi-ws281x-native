7e470874b5378891a3e2026cc48bb41a7efaf637
三浦勇 <c01150788f@edu.teu.ac.jp>
2020-04-11 19:50:33 +0900

    add rainbow.ts

---
+++ src/examples/rainbow.ts
@@ -0,0 +1,48 @@
+//https://github.com/beyondscreen/node-rpi-ws281x-native/blob/master/examples/rainbow.js
+import * as ws281x from 'rpi-ws281x-native'
+
+const NUM_LEDS = parseInt(process.argv[2], 10) || 10
+const pixelData = new Uint32Array(NUM_LEDS)
+
+ws281x.init(NUM_LEDS)
+
+// ---- trap the SIGINT and reset before exit
+process.on('SIGINT', () => {
+  ws281x.reset()
+  process.nextTick(() => { process.exit(0) })
+});
+
+
+// ---- animation-loop
+let offset = 0
+setInterval(() => {
+  for (var i = 0; i < NUM_LEDS; i++) {
+    pixelData[i] = colorwheel((offset + i) % 256)
+  }
+
+  offset = (offset + 1) % 256
+  ws281x.render(pixelData)
+}, 1000 / 30)
+
+console.log('Press <ctrl>+C to exit.')
+
+
+// rainbow-colors, taken from http://goo.gl/Cs3H0v
+function colorwheel(pos: number) {
+  pos = 255 - pos
+  if (pos < 85) {
+    return rgb2Int(255 - pos * 3, 0, pos * 3)
+  }
+  else if (pos < 170) {
+    pos -= 85
+    return rgb2Int(0, pos * 3, 255 - pos * 3)
+  }
+  else {
+    pos -= 170
+    return rgb2Int(pos * 3, 255 - pos * 3, 0)
+  }
+}
+
+function rgb2Int(r: number, g: number, b: number) {
+  return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff)
+}
