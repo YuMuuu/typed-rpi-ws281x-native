886824ba82d3bea74e9ca91ec39ce3c58aaed2a7
三浦勇 <c01150788f@edu.teu.ac.jp>
2020-04-11 19:09:28 +0900

    update declare module

--- src/@types/ws281x-native.d.ts
+++ src/@types/ws281x-native.d.ts
@@ -1,8 +1,10 @@
 // https://github.com/beyondscreen/node-rpi-ws281x-native
 
-export function init(numLeds: number, options?: Object): void;
-export function setIndexMapping(map: Array<number>): void;
-export function render(data: Uint32Array): Uint32Array;
-export function setBrightness(brightness: number): void;
-export function reset(): void;
-export function isStub(): boolean;
+declare module 'rpi-ws281x-native' {
+  export function init(numLeds: number, options?: Object): void;
+  export function setIndexMapping(map: Array<number>): void;
+  export function render(data: Uint32Array): Uint32Array;
+  export function setBrightness(brightness: number): void;
+  export function reset(): void;
+  export function isStub(): boolean;
+}
